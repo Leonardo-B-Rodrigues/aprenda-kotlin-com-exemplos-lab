6fd1e1ab6a0d07692c76ae5a26f7886c61056484
leobr <leobr2012@hotmail.com>
2023-09-22 19:57:13 -0300

    Entrega do desafio solicitado

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,42 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+// [Template no Kotlin Playground](https://pl.kotl.in/aG3pqT54E)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel {Básico, Intermediário, Difícil}
 
-class Usuario
+data class ConteudoEducacional(var nome: String, var duracaomin: Int, var professor: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class Formacao(var nome: String, var conteudos: List<ConteudoEducacional>, var nivel: Nivel)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+var inscritos = mutableListOf<String>()											//Criação da lista de usuarios
+val listainscritos: List<String> = inscritos									//Lista read-only de usuarios
 
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
-    }
+fun matricular(usuario: String) {												//Função para matricular novos usuarios
+    inscritos.add(usuario)
+}
+
+fun printlist(): List<String> {													//Função para visualizar lista completa
+    return listainscritos
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val aula1 = ConteudoEducacional("Introdução", 60, "Venilton")				//Criação de Conteúdo
+    val aula2 = ConteudoEducacional("Hello World", 60, "Venilton")
+    
+    val kotlin = Formacao("Kotlin", listOf(aula1, aula2), Nivel.Intermediário)	//Criação de Formação
+    
+    matricular("Leonardo")														//Ação de matricular
+    matricular("Fernando")
+    matricular("Paula")
+    
+    println("Lista de usuários inscritos:")
+    printlist().forEach {														//Visualização da lista de inscritos
+        usuario -> println(usuario)
+    }
+ 	
+    println()																	//Pula linha
+    println ("A formação em ${kotlin.nome}, de nível ${kotlin.nivel}, " +		//Imprime o cabeçalho da Formação
+             "é composta pelo(s) seguinte(s) conteúdo(s):")
+    for (conteudo in kotlin.conteudos) {
+    println(" - ${conteudo.nome}: Carga horária de ${conteudo.duracaomin} " + //Imprime o detalhamento dos conteudos
+             "min e ministrada por prof.(a) ${conteudo.professor}")
+    }
 }
